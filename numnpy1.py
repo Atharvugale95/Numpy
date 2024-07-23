2e6dbb52b0c83594950b93f5c120516b15833e58
Atharv Ugale <158261519+goldenknights95@users.noreply.github.com>
2024-07-23 03:45:43 -0700

    accessing two dimensional array using numpy

---
+++ numnpy1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+print(arr[1,1])
+
+print(arr[0,0:3])
+ 
+print(arr[1,0:3])
+
+print(arr)
